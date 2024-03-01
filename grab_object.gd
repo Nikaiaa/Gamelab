bd0b3b2e15c1d5a011973071463717b8aa0ed274
Frangypane <146567370+Frangypane@users.noreply.github.com>
2024-03-01 18:45:01 +0100

    grab object 2

--- grab_object.gd
+++ grab_object.gd
@@ -3,9 +3,17 @@ var mouse_in_zone_true
 var bribe1 = preload ("res://bribe1.tscn")
 var camera 
 var frag1
+var cameraPosition
+var cameraDirection
+var cameraRotation
+var spawnDistance 
+var spawnPos
+var look_at_matrix
+
 
 func _ready():
 	mouse_in_zone_true = false
+	camera = $"../char"
 	set_process_input(true)
 
 func _on_mouse_entered(): 
@@ -27,10 +35,17 @@ func _on_mouse_exited():
 	pass # Replace with function body.
 
 func _spawn_bribe():
-	camera = $"../char"
+	cameraPosition = $"../char".global_transform.origin
 	frag1 = $Sprite3D
+	cameraDirection = camera.global_transform.basis.z
+	spawnDistance = -2
+	spawnPos = cameraPosition + cameraDirection* spawnDistance
 	var instance = bribe1.instantiate()
 	print (camera)
-	instance.position.z = camera.position.z-2
+	instance.transform.origin = spawnPos
+	look_at_matrix = instance.global_transform.basis.looking_at(cameraDirection, Vector3(0, 1, 0))
+	instance.transform.basis = look_at_matrix
+	instance.scale =Vector3(4, 4, 4)
+	#instance.position.z = camera.position.z-2
 	add_child(instance)
 	frag1.queue_free()
