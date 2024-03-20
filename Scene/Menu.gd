30331ec20a58967b0ce744b65e9fe9905e6177f0
clementagathe <agathe.clement@edu.univ-fcomte.fr>
2024-03-20 11:30:29 +0100

    anim menu

--- Scene/Menu.gd
+++ Scene/Menu.gd
@@ -13,3 +13,35 @@ func _on_credit_pressed():
 
 func _on_quitter_pressed():
 	get_tree().quit()
+
+
+func _on_jouer_mouse_entered():
+	$AnimationPlayer.play("Slide")
+
+
+func _on_jouer_mouse_exited():
+	$AnimationPlayer.play_backwards("Slide")
+
+
+func _on_options_mouse_entered():
+	$AnimationPlayer.play("Slide_option")
+
+
+func _on_options_mouse_exited():
+	$AnimationPlayer.play_backwards("Slide_option")
+
+
+func _on_credit_mouse_entered():
+	$AnimationPlayer.play("Slide_Credit")
+
+
+func _on_credit_mouse_exited():
+	$AnimationPlayer.play_backwards("Slide_option")
+
+
+func _on_quitter_mouse_entered():
+	$AnimationPlayer.play("Slide_Quitter")
+
+
+func _on_quitter_mouse_exited():
+	$AnimationPlayer.play_backwards("Slide_Quitter")
