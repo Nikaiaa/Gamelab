df69309f3daa0c6b7b508cf9cc4b0138f98f27e9
Frangypane <146567370+Frangypane@users.noreply.github.com>
2024-04-04 21:59:37 +0200

    correction camera bribe + texture

--- Scene/boutonUI.gd
+++ Scene/boutonUI.gd
@@ -3,13 +3,25 @@ extends Node
 signal onSelfDestroy
 @onready var scene = $"."
 @export var narra : Label
+@onready var sub_viewport = $SubViewport
+@onready var mesh_instance_3d = $SubViewport/Node3D/MeshInstance3D
+@export var mesh_to_snapshot : ArrayMesh
+@export var snapshot_name : String
+
 
 func _ready():
-	narra.visible = true
-	var sceneBribe = get_node("root/Node3D/Bribe")
+	#mesh_instance_3d.mesh = mesh_to_snapshot
+	#await get_tree().create_timer(0.5).timeout
+	#var img = sub_viewport.get_viewport().get_texture().get_image()
+	#var image_path = "Bribes/bribesUI/%s.png" % snapshot_name
+	#img.save_png(image_path)
+	#narra.visible = true
 	set_process_input(true)
 	#narra.visible = true
 	#narra.text = sceneBribe.resource_data.resource.dialogue
+
+
+	
 
 func _unhandled_input(event):
 	if event is InputEventKey:
