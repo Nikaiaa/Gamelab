0cd07e98d2d98ed68009f518e21fe7484ef6fc0e
Nikaiaa <71067524+Nikaiaa@users.noreply.github.com>
2024-04-04 09:01:07 +0200

    Appart on fleek, animation et UI

--- Scene/UI.gd
+++ Scene/UI.gd
@@ -1,7 +1,7 @@
 extends Control
 
 var score = 0 
-@onready var label = $ColorRect/Label
+@onready var label = $ColorRect/Bribe
 
 func _input(event):
 	if event is InputEventMouseButton and event.is_action_pressed("left_click"):
@@ -10,7 +10,7 @@ func _input(event):
 		update_score_label()
 
 func update_score_label():
-	$ColorRect/Label.text = str(score)
+	$ColorRect/Bribe.text = str(score)
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
